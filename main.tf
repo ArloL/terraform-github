920083d38ce33067e6e2778b67d4bd1074371e9e
Arlo O'Keeffe <tiger@k5d.de>
2023-09-23 19:59:01 +0200

    add dotfiles

--- main.tf
+++ main.tf
@@ -10,3 +10,29 @@ terraform {
 # Configure the GitHub Provider
 provider "github" {
 }
+
+resource "github_repository" "dotfiles" {
+  name = "dotfiles"
+  description = "My collection of dotfiles used to configure my command line environments."
+  visibility = "public"
+  allow_merge_commit = false
+  allow_squash_merge = false
+  delete_branch_on_merge = true
+  has_downloads = true
+  has_issues = true
+  has_projects = true
+  has_wiki = true
+  vulnerability_alerts = true
+}
+
+resource "github_branch_default" "main" {
+  repository = github_repository.dotfiles.name
+  branch = "main"
+}
+
+resource "github_branch_protection" "default" {
+  repository_id = github_repository.dotfiles.node_id
+  pattern = github_branch_default.main.branch
+  force_push_bypassers = []
+  push_restrictions = []
+}
