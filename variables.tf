379e5956a4681efbebd3ca2b9df07a354fa92344
Arlo O'Keeffe <tiger@k5d.de>
2023-09-26 12:15:32 +0200

    update descriptions

--- variables.tf
+++ variables.tf
@@ -4,30 +4,30 @@ variable "repository" {
     default = {
         abenteuer-irland = {
             name = "abenteuer-irland"
-            description = "Mum's website for Abenteuer Irland."
+            description = "Mum's website for Abenteuer Irland"
             main_branch = "gh-pages"
             homepage = "https://arlol.github.io/abenteuer-irland/"
         }
         "arlol.github.io" = {
             name = "arlol.github.io"
-            description = ""
+            description = "This is the source of my GitHub page"
         }
         business-english = {
             name = "business-english"
-            description = "Mum's website for Business English."
+            description = "Mum's website for Business English"
             main_branch = "gh-pages"
         }
         chorito = {
             name = "chorito"
-            description = "A tool that does some chores in your source code."
+            description = "A tool that does some chores in your source code"
         }
         corporate-python = {
             name = "corporate-python"
-            description = ""
+            description = "A container for executing python in corporate environments"
         }
         dotfiles = {
             name = "dotfiles"
-            description = "My collection of dotfiles used to configure my command line environments."
+            description = "My collection of dotfiles used to configure my command line environments"
         }
         eclipse-projects = {
             name = "eclipse-projects"
@@ -36,15 +36,15 @@ variable "repository" {
         }
         gitfx = {
             name = "gitfx"
-            description = "A simple git client with Java 11+, JavaFX 15+ and GraalVM."
+            description = "A simple git client with Java 11+, JavaFX 15+ and GraalVM"
         }
         homebrew-tap = {
             name = "homebrew-tap"
-            description = "A homebrew tap for my own formulas and casks."
+            description = "A homebrew tap for my own formulas and casks"
         }
         menubar-scripts = {
             name = "menubar-scripts"
-            description = ""
+            description = "A collection of scripts that can run in e.g. xbar, swiftbar, etc."
         }
         mvnx = {
             name = "mvnx"
@@ -52,7 +52,7 @@ variable "repository" {
         }
         myprojects-cleaner = {
             name = "myprojects-cleaner"
-            description = ""
+            description = "A java application that runs git clean in a bunch of directories"
         }
         newlinechecker = {
             name = "newlinechecker"
@@ -60,43 +60,43 @@ variable "repository" {
         }
         nope-amine = {
             name = "nope-amine"
-            description = ""
+            description = "A firefox extension that slowly increases the time for things to load on reddit.com"
         }
         postgres-sync-demo = {
             name = "postgres-sync-demo"
-            description = "A demo on how to use triggers, queues, etc. to sync the app's data somewhere else."
+            description = "A demo on how to use triggers, queues, etc. to sync the app's data somewhere else"
         }
         python-nc = {
             name = "python-nc"
-            description = ""
+            description = "A test to see if I can implement nc's proxy functionality with python"
         }
         rss-to-mail = {
             name = "rss-to-mail"
-            description = "Read from RSS feeds and send an email for every new item."
+            description = "Read from RSS feeds and send an email for every new item"
         }
         sci-fi-movies = {
             name = "sci-fi-movies"
-            description = ""
+            description = "an app to import sci fi movies from rotten tomatoes into a database in order to run queries on them"
         }
         testcontainers-colima-github-actions = {
             name = "testcontainers-colima-github-actions"
-            description = ""
+            description = "A demo project of running testcontainers with colima on GitHub Actions"
         }
         vagrant-ssh-config = {
             name = "vagrant-ssh-config"
-            description = ""
+            description = "A vagrant plugin that automatically creates ssh configs for vms"
         }
         wait-for-ports = {
             name = "wait-for-ports"
-            description = "A command-line utility that waits until a port is open."
+            description = "A command-line utility that waits until a port is open"
         }
         webapp-classloader-test = {
             name = "webapp-classloader-test"
-            description = "This is a test that can be used during integration testing to check for classloader leaks."
+            description = "This is a test that can be used during integration testing to check for classloader leaks"
         }
         website-janitor = {
             name = "website-janitor"
-            description = "A set of tools that check websites for common misconfigurations or downtime."
+            description = "A set of tools that check websites for common misconfigurations or downtime"
         }
   }
 }
