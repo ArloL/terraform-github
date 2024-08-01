6eb6901e962fee36e667ab165257937656b9f054
Arlo O'Keeffe <tiger@k5d.de>
2024-08-01 12:29:22 +0200

    allow auto merge on all repositories

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -12,6 +12,7 @@ resource "github_repository" "repository" {
     has_wiki = true
     vulnerability_alerts = true
     archived = var.archived
+    allow_auto_merge = true
 
     dynamic "pages" {
         for_each = var.pages_build_type == null ? [] : [1]
