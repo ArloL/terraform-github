3e8c947149dc58bb09a8ac9274c504ba0ecb37dc
Arlo O'Keeffe <tiger@k5d.de>
2025-12-02 00:51:35 +0100

    set required status checks

--- variables.tf
+++ variables.tf
@@ -135,6 +135,11 @@ variable "repositories" {
         {
             name = "newlinechecker"
             description = "A sample project to play with GraalVM builds on GitHub Actions"
+            required_status_checks = [
+                "check-actions.required-status-check",
+                "codeql-analysis.required-status-check",
+                "main.required-status-check"
+            ]
             homebrew_tap_livecheck_pat = true
         },
         {
