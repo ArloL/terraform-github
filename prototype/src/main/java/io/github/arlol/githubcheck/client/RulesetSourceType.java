0bde92e98882b08060775af255f5403bd5c1951a
Arlo O'Keeffe <tiger@k5d.de>
2026-03-28 20:48:04 +0100

    remove prototype

--- prototype/src/main/java/io/github/arlol/githubcheck/client/RulesetSourceType.java
+++
@@ -1,9 +0,0 @@
-package io.github.arlol.githubcheck.client;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum RulesetSourceType {
-	@JsonProperty("Repository")
-	REPOSITORY, @JsonProperty("Organization")
-	ORGANIZATION
-}
